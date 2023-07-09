c7e897d24cc52e565eb4b299117e887ab13c5083
Lyle Wong <lylereturns@gmail.com>
2023-07-09 03:26:24 +0100

    Changes made: - "evaluate_accuracy" function no longer tracks losses and outputs the accuracies generated over the steps. - Accuracy during training on the train and validation splits is now being tracked. (Model alternates between training and evaluation mode) - Added additional plots to show the accuracy of the model during training and at inference. - Model is now evaluated after using model.eval() (For batch norm and drop out layers).

--- CNN.py
+++ CNN.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from data_handler import DataHandler
-from tools import evaluate_accuracy
+from tools import evaluate_accuracy, find_accuracy
 from matplotlib import pyplot as plt
 
 s_device = "cuda" if torch.cuda.is_available else "cpu" # Selected device
@@ -55,7 +55,7 @@ model = nn.Sequential(
                     nn.BatchNorm1d(5445),
                     nn.ReLU(),
 
-                    # nn.Dropout1d(p = 0.25, inplace = False),
+                    nn.Dropout1d(p = 0.25, inplace = False),
                     nn.Linear(5445, 5)
 
                     # 2
@@ -81,8 +81,11 @@ optimiser = torch.optim.AdamW(params = model.parameters(), lr = 0.0001)
 
 epochs = 1000
 batch_size = 50
+
 losses_i = []
 val_losses_i = []
+train_accuracy_i = []
+val_accuracy_i = []
 
 for i in range(epochs):
     
@@ -95,13 +98,31 @@ for i in range(epochs):
     # Cross entropy loss
     loss = F.cross_entropy(logits, Ytr)
 
-    # Validation forward pass
     with torch.no_grad():
+        
+        # Note: Must set to eval mode as BatchNorm layers and Dropout layers behave differently during training and evaluation
+        # BatchNorm layers - stops updating the moving averages in BatchNorm layers and uses running statistics instead of per-batch statistics
+        # Dropout layers - Dropout layers are de-activated during evaluation
+        model.eval()
+
+        # Train accuracy on current batch
+        preds = F.softmax(logits, dim = 1)
+        train_accuracy = find_accuracy(predictions = preds, targets = Ytr, batch_size = batch_size)
+        train_accuracy_i.append(find_accuracy(predictions = preds, targets = Ytr, batch_size = batch_size))
+
+        # Validation forward pass
         Xva, Yva = DH.generate_batch(batch_size, val_image_names)
         v_logits = model(Xva)
         v_loss = F.cross_entropy(v_logits, Yva)
         val_losses_i.append(v_loss.log10().item())
 
+        # Validation accuracy on current batch
+        v_preds = F.softmax(v_logits, dim = 1) # Softmax to find probability distribution
+        val_accuracy = find_accuracy(predictions = v_preds, targets = Yva, batch_size = batch_size)
+        val_accuracy_i.append(val_accuracy)
+
+        model.train()
+    
     # Zero-grad
     optimiser.zero_grad()
     
@@ -118,27 +139,76 @@ for i in range(epochs):
     losses_i.append(loss.log10().item())
 
     if i == 0 or (i + 1) % 10 == 0:
-        print(f"Epoch: {i + 1} | TrainLoss: {loss.item()} | ValLoss: {v_loss.item()}")
+        print(f"Epoch: {i + 1} | TrainLoss: {loss.item()} | ValLoss: {v_loss.item()} | TrainAcc: {train_accuracy} | ValAcc: {val_accuracy}")
 
-print("------------------------------------")
-print("Train accuracy")
-evaluate_accuracy(steps = 500, batch_size = 40, generate_batch_f = DH.generate_batch, model = model, image_names_split = train_image_names)
-print("------------------------------------")
-print("Val accuracy")
-evaluate_accuracy(steps = 500, batch_size = 40, generate_batch_f = DH.generate_batch, model = model, image_names_split = val_image_names)
+# Set model to evaluation mode (For dropout + batch norm layers)
+model.eval()
 
-losses_i = torch.tensor(losses_i).view(-1, 10).mean(1)
-val_losses_i = torch.tensor(val_losses_i).view(-1, 10).mean(1)
+# Plotting losses during training
+print("-----------------------------------------------------------------")
+print("Losses during training")
+
+A = 20
+losses_i = torch.tensor(losses_i).view(-1, A).mean(1)
+val_losses_i = torch.tensor(val_losses_i).view(-1, A).mean(1)
 
 fig, ax = plt.subplots()
-ax.plot([i for i in range(int(epochs / 10))], losses_i, label = "Train")
-ax.plot([i for i in range(int(epochs / 10))], val_losses_i, label = "Validation")
+ax.plot([i for i in range(int(epochs / A))], losses_i, label = "Train")
+ax.plot([i for i in range(int(epochs / A))], val_losses_i, label = "Validation")
 ax.legend()
 
-# plt.plot(losses_i)
 plt.show()
 
+# Plotting accuracies during training
+print("-----------------------------------------------------------------")
+print("Accuracy during training")
 
+B = 20
+train_accuracy_i = torch.tensor(train_accuracy_i).view(-1, B).mean(1)
+val_accuracy_i = torch.tensor(val_accuracy_i).view(-1, B).mean(1)
+
+fig, ax = plt.subplots()
+ax.plot([i for i in range(int(epochs / B))], train_accuracy_i, label = "Train")
+ax.plot([i for i in range(int(epochs / B))], val_accuracy_i, label = "Validation")
+ax.legend()
+plt.show()
+
+# Plotting accuracies after training
+print("-----------------------------------------------------------------")
+print("Accuracy after training")
+accuracy_steps = 1000
+accuracy_bs = 20
+C = 20
+CHECK_INTERVAL = 50
+train_accuracies = evaluate_accuracy(
+                                    steps = accuracy_steps, 
+                                    batch_size = accuracy_bs, 
+                                    generate_batch_f = DH.generate_batch, 
+                                    model = model, 
+                                    image_names_split = train_image_names, 
+                                    split_name = "Train",
+                                    check_interval = CHECK_INTERVAL
+                                    )
+                                    
+val_accuracies = evaluate_accuracy(
+                                    steps = accuracy_steps, 
+                                    batch_size = accuracy_bs, 
+                                    generate_batch_f = DH.generate_batch, 
+                                    model = model, 
+                                    image_names_split = val_image_names, 
+                                    split_name = "Val",
+                                    check_interval = CHECK_INTERVAL
+                                    )
+
+train_accuracies = torch.tensor(train_accuracies).view(-1, C).mean(1)
+val_accuracies = torch.tensor(val_accuracies).view(-1, C).mean(1)
+
+fig, ax = plt.subplots()
+ax.plot([i for i in range(int(accuracy_steps / C))], train_accuracies, label = "Train")
+ax.plot([i for i in range(int(accuracy_steps / C))], val_accuracies, label = "Validation")
+ax.legend()
+
+plt.show()
 
 # ------------------------------------------------------------------------
 # Tests:
@@ -219,3 +289,19 @@ plt.show()
 # Correct predictions: 15939 / 20000 | Accuracy(%): 79.69500000000001
 # Val accuracy
 # Correct predictions: 7491 / 20000 | Accuracy(%): 37.455
+
+# ------------------------------------
+# (Alternating between train and evaluation mode when finding accuracy and evaluation mode after training)
+
+# 1 [without dropout layer]
+
+# Epoch: 1000 | TrainLoss: 0.0008298794273287058 | ValLoss: 1.5287092924118042 | TrainAcc: 100.0 | ValAcc: 48.0
+# Correct predictions: 20000 / 20000 | TrainAccuracy(%): 100.0
+# Correct predictions: 8873 / 20000 | ValAccuracy(%): 44.365
+
+# ------------------------------------
+# 1 [with dropout layer (p = 0.25)]
+
+# Epoch: 1000 | TrainLoss: 0.40037113428115845 | ValLoss: 1.6299127340316772 | TrainAcc: 82.0 | ValAcc: 44.0
+# Correct predictions: 19932 / 20000 | TrainAccuracy(%): 99.66000000000001
+# Correct predictions: 8799 / 20000 | ValAccuracy(%): 43.995
